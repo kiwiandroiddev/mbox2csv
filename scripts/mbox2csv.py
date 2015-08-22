31a6ba6e44dc80a97fe63c39bb17ab95922b806b
Matt Clarke <kiwiandroiddev@gmail.com>
2015-08-22 23:27:27 +1200

    Message body extraction is more robust

--- scripts/mbox2csv.py
+++ scripts/mbox2csv.py
@@ -14,6 +14,14 @@ def print_progress(pct_progress):
     sys.stdout.write('\r[{0}] {1}%'.format('#'*(pct_progress/10), pct_progress))
     sys.stdout.flush()
 
+# Recurses down a message payload tree until a string is found
+def get_final_payload(msg):
+    if isinstance(msg, basestring):
+        return msg
+    if isinstance(msg.get_payload(), basestring):
+        return msg.get_payload()
+    return get_final_payload(msg.get_payload()[0])
+
 mbox_file = sys.argv[1]
 output_file = sys.argv[2] if len(sys.argv) > 2 else 'output.csv'
 
@@ -25,7 +33,7 @@ writer.writerow(['subject', 'from', 'date', 'message'])
 n = len(messages)
 print 'Writing messages...'
 for (i, msg) in enumerate(messages):
-    body = msg.get_payload()[0].get_payload()
+    body = get_final_payload(msg)
 
     # convert any date format to ISO!
     date = parser.parse(msg['date'])
